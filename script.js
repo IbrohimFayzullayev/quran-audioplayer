fe6d782ec13e9f77341270ee2ac9bc80f5f34c6f
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-04-09 22:16:03 +0500

    project finished

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+"use strict";
 const suralar = document.querySelector(".suralar");
 const box1 = document.querySelector(".box__1");
 const box2 = document.querySelector(".box__2");
 const arabicText = document.querySelector(".arabic__text");
-
+let audio = document.querySelector(".quranPlayer"),
+  surahsContainer = document.querySelector(".surahs"),
+  ayah = document.querySelector(".ayah"),
+  next = document.querySelector(".next"),
+  prev = document.querySelector(".prev"),
+  play = document.querySelector(".play");
 let addSura = async function () {
   let a = await fetch("https://api.quran.sutanlab.id/surah");
   let b = await a.json();
@@ -26,14 +32,76 @@ let addSura = async function () {
     let num = +target.closest(".row").id.split("_")[1];
     let k = await fetch(`https://api.quran.sutanlab.id/surah/${num}`);
     let c = await k.json();
-    let i = 1;
     let j = 1;
+    let h = 0;
     let verses = c.data.verses;
     for (let d of verses) {
-      let html2 = `<p class="text__1">${j++}. ${d.text.transliteration.en}</p>`;
+      let html2 = `<p class="txt__${h++} text__oyat">${j++}. ${
+        d.text.transliteration.en
+      }</p>`;
       englishText.insertAdjacentHTML("beforeend", html2);
     }
     impor(num);
+    let AyahsAudios = [];
+    let AyahsText = [];
+    verses.forEach((verse) => {
+      AyahsAudios.push(verse.audio.primary);
+      AyahsText.push(verse.text.arab);
+    });
+    let AyahIndex = 0;
+    changeAyah(AyahIndex);
+    audio.addEventListener("ended", () => {
+      AyahIndex++;
+      if (AyahIndex < AyahsAudios.length) {
+        changeAyah(AyahIndex);
+      } else {
+        AyahIndex = 0;
+        changeAyah(AyahIndex);
+        audio.pause();
+        // Swal.fire({
+        //   position: "center",
+        //   icon: "success",
+        //   title: "surah has been ended",
+        //   showConfirmButton: false,
+        //   timer: 1500,
+        // });
+        isPlaying = true;
+        togglePlay();
+      }
+    });
+    //Handle Next And Prev
+    next.addEventListener("click", () => {
+      AyahIndex < AyahsAudios.length - 1 ? AyahIndex++ : (AyahIndex = 0);
+      changeAyah(AyahIndex);
+      isPlaying = false;
+      togglePlay();
+    });
+    prev.addEventListener("click", () => {
+      AyahIndex == 0 ? (AyahIndex = AyahsAudios.length - 1) : AyahIndex--;
+      changeAyah(AyahIndex);
+      isPlaying = false;
+      togglePlay();
+    });
+    //handle Play And Pause Audio
+    let isPlaying = false;
+    togglePlay();
+    function togglePlay() {
+      if (isPlaying) {
+        audio.pause();
+        play.innerHTML = `<i class="fas fa-play"></i>`;
+        isPlaying = false;
+      } else {
+        audio.play();
+        play.innerHTML = `<i class="fas fa-pause"></i>`;
+        isPlaying = true;
+      }
+    }
+    play.addEventListener("click", togglePlay);
+    function changeAyah(index) {
+      belgila(index);
+      audio.src = AyahsAudios[index];
+      ayah.innerHTML = AyahsText[index];
+    }
   });
 };
 addSura();
@@ -44,11 +112,22 @@ let impor = function (id) {
     .then((res) => res.json())
     .then((res) => {
       let i = 1;
+      let y = 0;
       res.quran.forEach((element) => {
         if (element.chapter == id) {
-          let html1 = `<p class="text__1">${i++}. ${element.text}</p>`;
+          let html1 = `<p class="texts__${y++} text__oyat">${i++}. ${
+            element.text
+          }</p>`;
           arabicText.insertAdjacentHTML("beforeend", html1);
         }
       });
     });
+};
+let belgila = function (index) {
+  let uzbekcha = document.querySelector(`.texts__${index}`);
+  let en = document.querySelector(`.txt__${index}`);
+  en.style.backgroundColor = "#4343435c";
+  if (uzbekcha !== null) {
+    uzbekcha.style.backgroundColor = "#4343435c";
+  }
 };
